f03492a26cf9c31627af7229ba24774f77e2e8cf
EvgeniyKarachkov <cazinak341@gmail.com>
2021-06-03 23:16:05 +0300

    Finished project

--- SwiftBookRu_06 (WebKit)/ViewController.swift
+++ SwiftBookRu_06 (WebKit)/ViewController.swift
@@ -11,15 +11,24 @@ import WebKit
 
 class ViewController: UIViewController {
     @IBOutlet var webView: WKWebView!
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var forwardButton: NSLayoutConstraint!
+    @IBOutlet weak var urlTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        urlTextField.delegate = self
+        webView.navigationDelegate = self 
         
-        let url = URL(string: "https://www.apple.com")
+        let homePage = "https://www.apple.com"
+        let url = URL(string: homePage)
         let request = URLRequest(url: url!)
         
+        urlTextField.text = homePage
+        
         webView.load(request)
+        webView.allowsBackForwardNavigationGestures = true 
      
     }
     
@@ -28,6 +37,40 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func backButtonAction(_ sender: Any) {
+        if webView.canGoBack {
+        webView.goBack()
+        }
+        
+    }
+    
+    @IBAction func forwardButtonAction(_ sender: Any) {
+        if webView.canGoForward {
+        webView.goForward()
+        }
+    }
+    
+    
+}
+
+extension ViewController: UITextFieldDelegate, WKNavigationDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        
+        let urlString = textField.text!
+        let url = URL(string: urlString)!
+        let request = URLRequest(url: url)
+        
+        webView.load(request)
+        
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        urlTextField.text = webView.url?.absoluteString
+        backButton.isEnabled = webView.canGoBack
+        forwardButton.isActive = webView.canGoForward
+    }
     
 }
 
